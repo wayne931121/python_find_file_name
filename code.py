5b9c34a5b14da52559c04b59560a2fcd1e31db58
Wayne咏辰 <75261164+wayne931121@users.noreply.github.com>
2025-10-06 18:45:38 +0800

    Update code.py

--- code.py
+++ code.py
@@ -1,16 +1,19 @@
 import os, fnmatch
 
-def find(pattern, path):
+def find(pattern, path, prt=1):
     result = []
     for root, dirs, files in os.walk(path):
         for name in files:
             if fnmatch.fnmatch(name, pattern):
                 result.append(os.path.join(root, name))
+                if prt: print(result[-1])
         for name in dirs:
             if fnmatch.fnmatch(name, pattern):
                 result.append(os.path.join(root, name))
+                if prt: print(result[-1])
     return result
 
 find('*yourfilenameordictname*', 'base_path')
-find('*.txt', '/')
-find('*.jpg', '/content')
+find('*.jpg', r'C:\Users\原神\Downloads') #test on windows11
+find('*.txt', '/') #test on colab linux
+find('*.jpg', '/content') #test on colab linux
